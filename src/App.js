2c2d48ac1013e860e375d58424e0167b76f24cf2
Thomas Bleicher <tbleicher@gmail.com>
2017-12-03 09:00:56 -0500

    changed layout and cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route
-} from 'react-router-dom'
+} from 'react-router-dom';
 
 import logo from './logo.svg';
 import Calendar from './components/calendar';
@@ -10,19 +10,17 @@ import Calendar from './components/calendar';
 import './App.css';
 
 
-
 class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Route exact path="/" component={Calendar}/>
-        <Route path="/calendar/:year?/:month?" component={Calendar}/>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Calendar in React</h1>
+          </header>
+          <Route path="/calendar/:year?/:month?" component={Calendar}/>
+        </div>
       </Router>
     );
   }
